ad84da2d1935980be5a735abdb811518e06afdcd
hanguang254 <725204548@qq.com>
2024-02-04 16:52:24 +0800

    renew

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-    presets: ['next/babel'],
+export default module.exports = {
+    presets: [
+        [
+          'next/babel',
+          {
+            'preset-react': {
+              runtime: 'automatic',
+              importSource: '@emotion/react', // 根据你的需要，你可以更改这个导入源
+            },
+            'swc': true, // 启用 SWC
+          },
+        ],
+      ],
     plugins: [
       [
         'import',
