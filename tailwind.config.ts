1772f81a3ec2a7958986d95804bb98c5f0304f73
hanguang254 <725204548@qq.com>
2024-02-04 15:11:35 +0800

    renew

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,26 +2,6 @@ import type { Config } from "tailwindcss";
 import {nextui} from "@nextui-org/react";
 
 
-const nextobject = {
-  prefix: "nextui", // prefix for themes variables
-  addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-  defaultTheme: "dark", // default theme from the themes object
-  defaultExtendTheme: "light", // default theme to extend on custom themes
-  layout: {}, // common layout tokens (applied to all themes)
-  themes: {
-    light: {
-      layout: {}, // light theme layout tokens
-      colors: {}, // light theme colors
-    },
-    dark: {
-      layout: {}, // dark theme layout tokens
-      colors: {}, // dark theme colors
-    },
-    // ... custom themes
-  },
-}
-
-
 
 const config: Config = {
   content: [
@@ -41,7 +21,24 @@ const config: Config = {
   },
   darkMode: "class",
   plugins: [
-    nextui(nextobject)
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "dark", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {}, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {}, // dark theme colors
+        },
+        // ... custom themes
+      },
+    })
 ],
 };
 export default config;
