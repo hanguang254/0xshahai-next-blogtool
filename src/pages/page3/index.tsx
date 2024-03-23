4fe6e719b7c7f71bbccc791c6e6183879f315a1c
hanguang254 <725204548@qq.com>
2024-03-23 20:39:53 +0800

    增加Hyperlane的前端页面 暂未实现api方法” “

--- src/pages/page3/index.tsx
+++ src/pages/page3/index.tsx
@@ -1,13 +1,74 @@
 import React from 'react'
 import styles from './index.module.css'
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
+
+const rows = [
+  {
+    key: "1",
+    name: "Tony Reichert",
+    role: "CEO",
+    status: "Active",
+  },
+  {
+    key: "2",
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    status: "Paused",
+  },
+  {
+    key: "3",
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    status: "Active",
+  },
+  {
+    key: "4",
+    name: "William Howard",
+    role: "Community Manager",
+    status: "Vacation",
+  },
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "role",
+    label: "ROLE",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+];
 
 export default function Tool() {
+  const [selectedKeys, setSelectedKeys]:[selectedKeys:any, setSelectedKeys:any] = React.useState(new Set(["2"]));
+
   return (
     <div className={styles.main}>
       <div className={styles.content}>
         正在开发......
       </div>
-      
+      <Table 
+        aria-label="Controlled table example with dynamic content"
+        selectionMode="multiple"
+        selectedKeys={selectedKeys}
+        onSelectionChange={setSelectedKeys}
+      >
+        <TableHeader columns={columns}>
+          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+        </TableHeader>
+        <TableBody items={rows}>
+          {(item) => (
+            <TableRow key={item.key}>
+              {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
     </div>
   )
 }
--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -1,13 +1,94 @@
 import React from 'react'
 import styles from './index.module.css'
+import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
+
+const rows = [
+  {
+    key: "1",
+    count: 1,
+    day: 1,
+    month:1,
+    years:1,
+    address:"0x88A68278fE332846BACC78BB6c38310a357BEe06",
+  },
+  {
+    key: "2",
+    count: 1,
+    day: 1,
+    month:1,
+    years:1,
+    address:"0x88A68278fE332846BACC78BB6c38310a357BEe06"
+  }
+];
+
+const columns = [
+  {
+    key: "address",
+    label: "ADDRESS",
+    // width:550
+  },
+  {
+    key: "count",
+    label: "跨链次数",
+    // width:600
+  },
+  {
+    key: "day",
+    label: "活跃天数",
+    // width:40
+  },
+  {
+    key: "month",
+    label: "活跃月数",
+    // width:40
+  },
+  {
+    key: "years",
+    label: "活跃年数",
+    // width:40
+  }
+];
+
+type SelectedKey=[selectedKeys:any, setSelectedKeys:any]
 
 export default function Tool() {
+  const [selectedKeys, setSelectedKeys]:SelectedKey= React.useState(new Set([""]));
+
+
   return (
     <div className={styles.main}>
-      <div className={styles.content}>
+      {/* <div className={styles.content}>
         正在开发......
-      </div>
-      
+      </div> */}
+      <Tabs aria-label="Options" size='lg' fullWidth={true} radius='full'>
+        <Tab key="hyperlane" title="Hyperlane查询" className={styles.HyperlaneTab}>
+          <Table 
+            aria-label="Controlled table example with dynamic content"
+            selectionMode="multiple"
+            selectedKeys={selectedKeys}
+            onSelectionChange={setSelectedKeys}
+          >
+            <TableHeader columns={columns}>
+              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={rows}>
+              {(item) => (
+                <TableRow key={item.key}>
+                  {(columnKey) => <TableCell width={1000}>{getKeyValue(item, columnKey)}</TableCell>}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          {/* 数据操作 */}
+        </Tab>
+        <Tab key="music" title="开发中..." isDisabled>
+          开发中... 
+        </Tab>
+        <Tab key="videos" title="开发中..." isDisabled>
+          开发中... 
+        </Tab>
+      </Tabs>
     </div>
   )
 }
