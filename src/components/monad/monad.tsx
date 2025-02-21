7c5b09d0648fcb48eb67403a08023166dcfde307
hanguang254 <725204548@qq.com>
2025-02-21 16:13:40 +0800

    renew

--- src/components/monad/monad.tsx
+++ src/components/monad/monad.tsx
@@ -154,6 +154,7 @@ export default function Monad() {
             isOpen={isOpen} 
             onOpenChange={onOpenChange}
             isDismissable={false}
+            placement="center"
         >
             <ModalContent>
                 {(onClose) => (
