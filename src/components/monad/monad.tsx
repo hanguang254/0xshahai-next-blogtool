fc532a77dea20a9d24ddbbdef21aca5af861e6ee
hanguang254 <725204548@qq.com>
2025-02-24 17:34:57 +0800

    renew

--- src/components/monad/monad.tsx
+++ src/components/monad/monad.tsx
@@ -8,13 +8,15 @@ import {
     Button,
     useDisclosure,
     useDraggable,
-    Textarea
+    Textarea,
+    Form
 } from "@heroui/react";
 
 import {Input} from "@heroui/input";
 
 import {Card, CardBody, CardFooter, Image} from "@heroui/react";
 import styles from "./index.module.css"
+import { log } from 'console';
 
 // 为不同的模态框创建不同的内容组件
 const Monad1Content = ({ onClose ,...moveProps}) => {
@@ -25,6 +27,28 @@ const Monad1Content = ({ onClose ,...moveProps}) => {
     // 判断有效
     const [isValid, setIsValid] = React.useState(true);
     const [errorMessage, setErrorMessage] = React.useState("");
+    //判断数量输入框是否为空
+    const [isAmountValid, setIsAmountValid] = React.useState(true); 
+
+
+    // 存款金额
+    const [amount, setAmount] = React.useState("");
+    const [amountlist, setAmountList] = React.useState([]);
+    // 金额输入框错误消息
+    const [amounterrormessage,setAmountErrorMessage] =React.useState("");
+    
+
+    // 分发金额状态
+    const [submitted, setSubmitted] = React.useState(null);
+
+    // 分发金额
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.currentTarget));
+    
+        setSubmitted(data);
+    };
+
 
     //获取输入的地址数组
     const handleAction = () => {
@@ -48,19 +72,52 @@ const Monad1Content = ({ onClose ,...moveProps}) => {
         setErrorMessage("");
         return true;
     };
-
+    //地址输入
     const handleInputChange = (e) => {
         const newValue = e.target.value;
         setInputValue(newValue);
         validateAddresses(newValue);
     };
+    //数量输入
+    const getInputValue = (e) => {
+        // 获取输入的值
+        const value = e.target.value;
+        console.log("原始输入值:", value);
+    
+        // 将输入值按逗号分隔并转换为数组
+        const valueArray = value.split(',').map(item => item.trim());
+    
+        // 打印转换后的数组
+        console.log("转换后的数组:", valueArray);
+    
+        // 更新状态
+        setAmount(value);
+        setAmountList(valueArray)
+    };
+
+
+    //存款按钮判断是否为空
+    const handleDeposit = () => {
+        console.log(amount);
+        
+        if (!amount.trim()) {
+            setIsAmountValid(false)
+            setIsAmountValid(false);
+            setAmountErrorMessage('金额不能为空！');
+        } else {
+            setAmountErrorMessage('');
+            setIsAmountValid(true);
+            setIsAmountValid(true)
+        }
+    };
 
     return (
         <>
+        <Form className="h-full" onSubmit={onSubmit}>
             <ModalHeader {...moveProps} className="flex flex-col gap-1">
                 MON合约分发工具
             </ModalHeader>
-            <ModalBody>
+            <ModalBody className='w-full'>
                 <Textarea
                     disableAnimation
                     // disableAutosize
@@ -85,10 +142,10 @@ const Monad1Content = ({ onClose ,...moveProps}) => {
                 <div>
                     <Input
                         
-                        isRequired
-                        label="分发数量"
+                        // isRequired
+                        label="存款金额"
                         labelPlacement="outside"
-                        placeholder="0.00"
+                        errorMessage={amounterrormessage}
                         startContent={
                             <div className="pointer-events-none flex items-center">
                                 <span className="text-default-400 text-small">$</span>
@@ -97,21 +154,53 @@ const Monad1Content = ({ onClose ,...moveProps}) => {
                         type="number"
                         min="0.01"
                         step="0.01"
+                        onChange={getInputValue}
+                        value={amount}
+                        isInvalid={!isAmountValid}
+                        color={isAmountValid ? "default" : "danger"}
+                    />
+                </div>
+                <div className="my-1" />
+                <div>
+                    <Input
+                        
+                        isRequired
+                        label="分发金额"
+                        labelPlacement="outside"
+                        errorMessage={amounterrormessage}
+                        startContent={
+                            <div className="pointer-events-none flex items-center">
+                                <span className="text-default-400 text-small">$</span>
+                            </div>
+                        }
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        validate={(value) => {
+                            if (value.length <= 0) {
+                              return "转发金额不能为空";
+                            }
+                        }}
                     />
                 </div>
             </ModalBody>
             <ModalFooter className={styles.buttonContainer}>
-                <Button color="primary"  onPress={onClose}>
+                <Button color="primary" 
+                    onPress={handleDeposit}
+                    isDisabled={!isAmountValid || !amount.trim()}
+                 >
                     存款
                 </Button>
                 <Button 
                     color="primary" 
                     onPress={handleAction}
                     isDisabled={!isValid || !inputValue.trim()}
+                    type='submit'
                 >
-                    Action
+                    发送
                 </Button>
             </ModalFooter>
+            </Form>
         </>
     );
 };
